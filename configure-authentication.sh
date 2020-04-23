33b1c144ee3bf72450069c5ac14c5ae9cf5074f3
Tony Elmore <aelmore@pivotal.io>
2020-04-23 11:01:33 -0700

    try passing env vars for env.yml

--- configure-authentication.sh
+++ configure-authentication.sh
@@ -3,7 +3,10 @@ docker run -it --rm \
   -v $PWD/harness-platform-automation:/automation \
   -w /workspace \
   --env-file harness-platform-automation/vars/config-auth.yml \
-  -e ENV_FILE=secrets/env.yml \
+  -e ENV_FILE=env.yml \
+  -e OM_TARGET=${OM_TARGET} \
+  -e OM_USERNAME=${OM_USERNAME} \
+  -e OM_PASSWORD=${OM_PASSWORD} \
   -e AUTH_CONFIG_FILE=secrets/auth.yml \
   platform-automation-toolkit-image:${TOOLKIT_IMAGE_VERSION} \
     /bin/bash ./harness-platform-automation/platform-automation-tasks/tasks/configure-authentication.sh
