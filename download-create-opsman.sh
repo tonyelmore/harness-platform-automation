5e0455c361cd6505aa27717025377b8eb7dc4307
Tony Elmore <aelmore@pivotal.io>
2020-04-23 16:58:32 -0700

    Use secrets in create-vm

--- download-create-opsman.sh
+++ download-create-opsman.sh
@@ -5,6 +5,7 @@ docker run -it --rm \
   --env-file $SCRIPT_REPO/vars/download-create-opsman.yml \
   -e STATE_FILE=$config_repo/environments/$iaas/$foundation/state/state.yml \
   -e OPSMAN_CONFIG_FILE=$config_repo/environments/$iaas/$foundation/config-director/templates/opsman.yml \
+  -e SECRET_CONFIG_FILE=$config_repo/environments/$iaas/$foundation/config-director/secrets/director.yml \
   -e SCRIPT_REPO=$SCRIPT_REPO \
   platform-automation-toolkit-image:${TOOLKIT_IMAGE_VERSION} \
     /bin/bash $SCRIPT_REPO/platform-automation-tasks/tasks/create-vm.sh
