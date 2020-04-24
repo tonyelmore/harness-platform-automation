df45e8383b311c801955b4c53175bc4ee932f9ba
Tony Elmore <aelmore@pivotal.io>
2020-04-24 10:28:10 -0700

    quote params

--- configure-authentication.sh
+++ configure-authentication.sh
@@ -1,18 +1,3 @@
-echo "Starting docker run"
-echo $SCRIPT_REPO
-echo $OM_TARGET
-echo $OM_USERNAME
-echo $OM_PASSWORD
-echo $iaas
-echo $foundation
-echo $PCF_opsman_password
-echo $PCF_opsman_decryption_passphrase
-echo $PCF_ops_manager_iam_user_access_key
-echo $PCF_ops_manager_iam_user_secret_key
-echo $PCF_ops_manager_ssh_public_key
-echo $PCF_ops_manager_ssh_private_key
-echo ${TOOLKIT_IMAGE_VERSION}
-
 set -x
 
 docker run -it --rm \
@@ -25,12 +10,12 @@ docker run -it --rm \
   -e OM_PASSWORD=${OM_PASSWORD} \
   -e OM_SKIP_SSL_VALIDATION=true \
   -e AUTH_CONFIG_FILE=$config_repo/environments/$iaas/$foundation/config-director/secrets/auth.yml \
-  -e PCF_opsman_username=${PCF_opsman_username} \
-  -e PCF_opsman_password=${PCF_opsman_password} \
-  -e PCF_opsman_decryption_passphrase=${PCF_opsman_decryption_passphrase} \
-  -e PCF_ops_manager_iam_user_access_key=${PCF_ops_manager_iam_user_access_key} \
-  -e PCF_ops_manager_iam_user_secret_key=${PCF_ops_manager_iam_user_secret_key} \
-  -e PCF_ops_manager_ssh_public_key=${PCF_ops_manager_ssh_public_key} \
-  -e PCF_ops_manager_ssh_private_key=${PCF_ops_manager_ssh_private_key} \
-  platform-automation-toolkit-image:${TOOLKIT_IMAGE_VERSION} \
+  -e PCF_opsman_username="${PCF_opsman_username}" \
+  -e PCF_opsman_password="${PCF_opsman_password}" \
+  -e PCF_opsman_decryption_passphrase="${PCF_opsman_decryption_passphrase}" \
+  -e PCF_ops_manager_iam_user_access_key="${PCF_ops_manager_iam_user_access_key}" \
+  -e PCF_ops_manager_iam_user_secret_key="${PCF_ops_manager_iam_user_secret_key}" \
+  -e PCF_ops_manager_ssh_public_key="${PCF_ops_manager_ssh_public_key}" \
+  -e PCF_ops_manager_ssh_private_key="${PCF_ops_manager_ssh_private_key}" \
+  platform-automation-toolkit-image:"${TOOLKIT_IMAGE_VERSION}" \
     /bin/bash $SCRIPT_REPO/platform-automation-tasks/tasks/configure-authentication.sh
