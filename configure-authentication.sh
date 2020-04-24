99b496fd3a8b59f03fbbfa53ffb69f58eec11f9a
Tony Elmore <aelmore@pivotal.io>
2020-04-23 23:57:34 -0700

    Auth config interpolate

--- configure-authentication.sh
+++ configure-authentication.sh
@@ -8,5 +8,11 @@ docker run -it --rm \
   -e OM_PASSWORD=${OM_PASSWORD} \
   -e OM_SKIP_SSL_VALIDATION=true \
   -e AUTH_CONFIG_FILE=$config_repo/environments/$iaas/$foundation/config-director/secrets/auth.yml \
+  -e PCF_opsman_password=${PCF_opsman_password} \
+  -e PCF_opsman_decryption_passphrase=${PCF_opsman_decryption_passphrase} \
+  -e PCF_ops_manager_iam_user_access_key=${PCF_ops_manager_iam_user_access_key} \
+  -e PCF_ops_manager_iam_user_secret_key=${PCF_ops_manager_iam_user_secret_key} \
+  -e PCF_ops_manager_ssh_public_key=${PCF_ops_manager_ssh_public_key} \
+  -e PCF_ops_manager_ssh_private_key=${PCF_ops_manager_ssh_private_key} \  
   platform-automation-toolkit-image:${TOOLKIT_IMAGE_VERSION} \
     /bin/bash $SCRIPT_REPO/platform-automation-tasks/tasks/configure-authentication.sh
--- platform-automation-tasks/tasks/configure-authentication.sh
+++ platform-automation-tasks/tasks/configure-authentication.sh
@@ -10,8 +10,11 @@ do
   vars_files_args+=("--vars-file ${vf}")
 done
 
-mkdir interpolated_files
-om interpolate -c ${AUTH_CONFIG_FILE} > interploated_files/${AUTH_CONFIG_FILE}
+f=interpolated_files/${AUTH_CONFIG_FILE}
+mkdir -p "${f%/*}"
+export OM_VARS_ENV=PCF
+om interpolate -c ${AUTH_CONFIG_FILE} > $f
+
 
 # ${vars_files_args[@] needs to be globbed to pass through properly
 # shellcheck disable=SC2068
